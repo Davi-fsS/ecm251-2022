3a0b5cb38e7203593169de59adcdc0039637038d
Davi-fsS <davifssoares2002@gmail.com>
2022-08-25 10:56:04 -0300

    manipulacao de arquivos

--- Teoria/Aula18/main.py
+++ Teoria/Aula18/main.py
@@ -1,19 +1,30 @@
 #Código para escrever dados em um arquivo
 
 import os
+from traceback import print_stack
 
+try:
+    arquivo = open("data/sistemas.txt","a")     #Cria arquivo novo, importante: "a" é de append
 
-arquivo = open("data/sistemas.txt","a")     #Cria arquivo novo, importante: "a" é de append
+    continuar = True
 
-continuar = True
+    while continuar:
+        os_name = input("Diga um OS: ")
 
-while continuar:
-    os_name = input("Diga um OS: ")
+        if(not os_name):                      #Verificar se a string ta vazia ou não 
+            continuar = False
+            continue
+
+        arquivo.write(f'{os_name}\n')
+
+    arquivo.close()
+
+except FileNotFoundError:
+    print("Caminho não existe, favor verificar.")
+
+except Exception:
+    print("Algo não esperado ocorreu: ")
+    print_stack()
     
-    if(not os_name):                      #Verificar se a string ta vazia ou não 
-        continuar = False
-        continue
-
-    arquivo.write(f'{os_name}\n')
-
-arquivo.close()
+finally:
+    print("Chegamos no final pessoal!")
