6fe10f53dc54ef128f488a45af5e5e24629af91a
Davi-fsS <davifssoares2002@gmail.com>
2022-10-06 11:18:51 -0300

    criacao do viewer e declaracao no main

--- Teoria/aula21/src/controllers/tarefa_controller.py
+++ Teoria/aula21/src/controllers/tarefa_controller.py
@@ -9,6 +9,7 @@ class TarefaController:
     def criar_nova_tarefa(self, descricao):
         tarefa = Tarefa(descricao)                 # tarefa vai receber a classe Tarefa com uma descricao
         self._lista_de_tarefas.append(tarefa)      # adicionando essa variavel para a lista
+        print(self._lista_de_tarefas)
 
     def exibir_total_de_tarefas(self):
         return len(self._lista_de_tarefas)
